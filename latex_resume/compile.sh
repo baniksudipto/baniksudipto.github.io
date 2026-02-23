233b27ab0b7b7583e57f1b0f7a9790de5a42677d
baniksudipto <sudipta.banik.112@gmail.com>
2026-02-24 01:48:19 +0530

    fix work break issues

--- latex_resume/compile.sh
+++ latex_resume/compile.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-pdflatex -interaction=nonstopmode sudipta_banik_software_engineer_resume.tex
-pdflatex -interaction=nonstopmode sudipta_banik_software_engineer_resume.tex
 
-cp sudipta_banik_software_engineer_resume.pdf ../sudipta_banik_software_engineer_resume.pdf
-cp sudipta_banik_software_engineer_resume.pdf ~/Downloads/Resumes/sudipta_banik_software_engineer_resume.pdf
+set -e
+
+TEX="sudipta_banik_software_engineer_resume"
+PDF_OUT="sudipta_banik_software_engineer_resume.pdf"
+
+# Compile (twice for correct layout/links)
+pdflatex -interaction=nonstopmode "$TEX.tex"
+pdflatex -interaction=nonstopmode "$TEX.tex"
+
+# Cleanup aux files
+rm -f "$TEX.aux" "$TEX.log" "$TEX.out"
+
+# Copy to destinations
+mkdir -p ~/Downloads/Resumes
+cp "$TEX.pdf" "../$PDF_OUT"
+cp "$TEX.pdf" ~/Downloads/Resumes/"$PDF_OUT"
+
+echo "Done. PDF copied to ../ and ~/Downloads/Resumes/"
